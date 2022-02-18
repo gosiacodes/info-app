9b5b0bfa75721b605bea0235c1c52141b2a7ffa4
Gosia <margareta.xx@gmail.com>
2022-02-18 11:18:17 +0100

    Fix app-layout
    Change layout in HTML and CSS, set flex, fix responsivity, change UI colours and appearance.

--- www/app.js
+++ www/app.js
@@ -76,6 +76,8 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
     accessToken: 'pk.eyJ1IjoiZ3JlZW55NzMiLCJhIjoiY2szNXFhY3B4MWVoeTNobzJ0cjBrenl1biJ9.82vZeA5kvvzOlk2lFXlXlw'
 }).addTo(map);
 
